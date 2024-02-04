dd4335b739e000e8821b82fef5ac8a1ba834a811
Alex Raymond <alexjacobraymond@gmail.com>
2024-02-04 13:45:05 -0800

    working on hero page

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Button } from './Button';
+import Image from 'next/image';
 
 const Hero: React.FC = () => {
     return (
-      <div className="bg-cover bg-center flex justify-center items-center mt-10">
+      <div className="container mx-auto flex min-h-screen flex-col items-center max-w-screen-md mt-10">
         <div className="bg-opacity-50 p-6 rounded-lg">
-          <h1 className="text-6xl mb-4">Everything in Life is an Adventure</h1>
-          <p className="text-lg mb-8">Discover amazing things with us</p>
+          <div>
+            <h1 className="text-6xl mb-4">Life is Boring.</h1>
+            <p className="text-lg mb-8">Turn it into some quests, like a wizard would give you or something</p>
+          </div>
+          <Image       
+            src="/../public/images/hero-image-1.png"
+            width={500}
+            height={500}
+            alt="Picture of the author" 
+            className=''
+      />
         </div>
       </div>
     );
