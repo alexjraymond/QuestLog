ac757f505c8ee97dd8594931ab138595b8848bd7
Alex Raymond <alexjacobraymond@gmail.com>
2024-02-04 14:06:43 -0800

    landing page responsiveness

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Hero: React.FC = () => {
     return (
       <div className="container mx-auto flex min-h-screen flex-col items-center max-w-screen-md mt-10">
-        <div className="bg-opacity-50 p-6 rounded-lg">
+        <div className="mx-auto bg-opacity-50 p-6 rounded-lg flex-wrap justify-center lg">
           <div>
             <h1 className="text-6xl mb-4">Life is Boring.</h1>
             <p className="text-lg mb-8">Turn it into some quests, like a wizard would give you or something</p>
